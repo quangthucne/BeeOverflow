555a49c9d94b90de00f3db77c4fe06b04b8fe809
quangthucne <thucbqpc08717@gmail.com>
2025-04-01 20:05:30 +0700

    init BE springboot

---
+++ ApiBeeOverflow/src/main/java/com/beeoverflow/apibeeoverflow/ApiBeeOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.beeoverflow.apibeeoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeeOverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBeeOverflowApplication.class, args);
+    }
+
+}
---
+++ ApiBeeOverflow/src/test/java/com/beeoverflow/apibeeoverflow/ApiBeeOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beeoverflow.apibeeoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBeeOverflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

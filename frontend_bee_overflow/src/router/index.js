d19dd8e8466ee86800ff66919ab10fff78594e7d
ViKhiem <163488262+ViKhiem@users.noreply.github.com>
2025-04-07 13:57:25 +0700

    index.js , main.js, app.vue, pakage

--- frontend_bee_overflow/src/router/index.js
+++ frontend_bee_overflow/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import DocumentView from '@/views/DocumentView.vue'
+import TagView from '@/views/TagView.vue'
+import ListUserView from '@/views/ListUserView.vue'
+import UserinfoView from '@/views/UserinfoView.vue'
+import QuestionView from '@/views/QuestionView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,12 +16,39 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/questions',
+      name: 'questions',
+      component: QuestionView,
+    },
+    {
+      path: '/userinfo',
+      name: 'userinfo',
+      component: UserinfoView,
+    },
+    {
+      path: '/listuser',
+      name: 'listuser',
+      component: ListUserView,
+    },
+    {
+      path: '/tag',
+      name: 'tag',
+      component: TagView,
+    },
+    {
+      path: '/document',
+      name: 'document',
+      component: DocumentView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
   ],
 })

28da9b2cd792119b9bb1ceb6c32e6cdb747557bf
ViKhiem <163488262+ViKhiem@users.noreply.github.com>
2025-04-07 13:56:33 +0700

    user info
    view

---
+++ frontend_bee_overflow/src/views/UserinfoView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import UserInfo from '@/components/User/UserInfo.vue'
+import HeaDer from '@/components/User/HeaDer.vue';
+import FooTer from '@/components/User/FooTer.vue';
+</script>
+
+<template>
+
+<header>
+    <HeaDer />
+</header>
+
+  <main>
+<UserInfo />
+</main>
+
+<footer>
+    <FooTer />
+</footer>
+
+</template>
